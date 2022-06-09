3fe6c89cc9fce3240adebfbf36d50876361aa96f
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-09 23:55:30 +0700

    Create a extension method registering AspNet Core Identity services for DI

---
+++ src/Infrastructure/Dependencies.cs
@@ -0,0 +1,24 @@
+﻿using ClubApp.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClubApp.Infrastructure
+{
+    public static class Dependencies
+    {
+        public static IdentityBuilder AddAppIdentity(this IServiceCollection services, string connectionString)
+        {
+            return services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            })
+                .AddIdentity<ApplicationUser, ApplicationRole>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                })
+                .AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<AppIdentityDbContext>();
+        }
+    }
+}
