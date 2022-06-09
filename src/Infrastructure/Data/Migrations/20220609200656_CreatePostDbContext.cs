81019a8b9b2aebe2bc5fdfc107881ed278c65194
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-10 03:15:05 +0700

    Add migration for PostDbContext

---
+++ src/Infrastructure/Data/Migrations/20220609200656_CreatePostDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubApp.Infrastructure.Data.Migrations
+{
+    public partial class CreatePostDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+        }
+    }
+}
