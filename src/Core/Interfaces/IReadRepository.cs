ed005bc7f124465e471bfcda7346ed8fbf259dec
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-11 14:16:36 +0700

    Create Repository Interface based of Ardalis.Specification

---
+++ src/Core/Interfaces/IReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ardalis.Specification;
+
+namespace ClubApp.Core.Interfaces
+{
+    public interface IReadRepository<T> : IDisposable, IReadRepositoryBase<T> where T : class
+    {
+    }
+}
---
+++ src/Core/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,8 @@
+﻿using Ardalis.Specification;
+
+namespace ClubApp.Core.Interfaces
+{
+    public interface IRepository<T> : IDisposable, IRepositoryBase<T> where T : class
+    {
+    }
+}
