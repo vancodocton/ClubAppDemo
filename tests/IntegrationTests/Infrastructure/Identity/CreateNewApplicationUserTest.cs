17b5b3fb0faa1553dd6b2fcdff2308f24a6a5d50
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-12 01:55:43 +0700

    update draft works

---
+++ tests/IntegrationTests/Infrastructure/Identity/CreateNewApplicationUserTest.cs
@@ -0,0 +1,25 @@
+﻿using ClubApp.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace IntegrationTests.Infrastructure.Identity
+{
+    
+    public class CreateNewApplicationUserTest
+    {
+        
+        public CreateNewApplicationUserTest()
+        {            
+        }
+
+        [Fact]
+        public void CreateNewUserInstance()
+        {
+            var user1 = new ApplicationUser("newusername");
+
+            var user2 = new ApplicationUser();
+
+            Assert.IsType<ApplicationUser>(user1);
+            Assert.IsType<ApplicationUser>(user2);
+        }
+    }
+}
---
+++ tests/UnitTests/Entities/PostAggregrate/CreateNewPostInstance.cs
@@ -0,0 +1,20 @@
+﻿using ClubApp.Core.Entities.PostAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Entities.PostAggregrate
+{
+    public class CreateNewPostInstance
+    {
+        [Fact]
+        public void CreateNewPost()
+        {
+            string userId = "userId";
+            string title = "Post Title";
+            var post = new Post(userId, title);
+        }
+    }
+}
