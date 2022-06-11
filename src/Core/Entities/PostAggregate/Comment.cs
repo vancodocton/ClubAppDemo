5816d8cf581c60d08b06740564b1fa63940aa9b2
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-11 15:45:00 +0700

    Apply Aggregrate Root pattern to Entity models

---
+++ src/Core/Entities/PostAggregate/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace ClubApp.Core.Entities.PostAggregate
+{
+    public class Comment : EntityBase<int>
+    {
+        public string Content { get; private set; }
+
+        public Comment(string content)
+        {
+            Content = content;
+        }
+    }
+}
---
+++ src/Core/Entities/PostAggregate/Post.cs
@@ -0,0 +1,17 @@
+﻿using ClubApp.Core.Interfaces;
+
+namespace ClubApp.Core.Entities.PostAggregate
+{
+    public class Post : EntityBase<int>, IAggregateRoot
+    {
+        public string Title { get; private set; }
+
+        public virtual ICollection<Comment> Comments { get; private set; }
+
+        public Post(string title, ICollection<Comment>? comments = null)
+        {
+            Title = title;
+            Comments = comments ?? new List<Comment>();
+        }
+    }
+}
