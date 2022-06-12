a25fa85931afc07e185e84a4228fec24966a8e0e
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-06-12 23:26:38 +0700

    pull methods from PostService to IPostService and implement Idispose pattern

--- src/Core/Interfaces/IPostService.cs
+++ src/Core/Interfaces/IPostService.cs
@@ -2,8 +2,12 @@
 
 namespace ClubApp.Core.Interfaces
 {
-    public interface IPostService
+    public interface IPostService : IDisposable
     {
         Task<Post> CreatePostAsync(UserId userId, string title);
+        
+        Task<Comment> CreateCommentAsync(string userId, int postId, string content, CancellationToken token = default);
+        
+        Task<ICollection<Comment>> GetCommentsByPostId(int postId, int take = 10, CancellationToken token = default);
     }
 }
--- src/Core/Services/PostService.cs
+++ src/Core/Services/PostService.cs
@@ -4,10 +4,11 @@ using Microsoft.Extensions.Logging;
 
 namespace ClubApp.Core.Services
 {
-    public class PostService : IPostService
+    public class PostService : IPostService, IDisposable
     {
         private readonly ILogger<PostService> logger;
         private readonly IPostRepository postRepository;
+        private bool disposedValue;
 
         public PostService(
             ILogger<PostService> logger,
@@ -35,6 +36,31 @@ namespace ClubApp.Core.Services
         public Task<ICollection<Comment>> GetCommentsByPostId(PostId postId, int take = 10, CancellationToken token = default)
         {
             return postRepository.GetCommentsByIdAsync(postId, take, token);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    postRepository.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~PostService()
+        {
+            Dispose(disposing: false);
+        }
+
+        void IDisposable.Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
